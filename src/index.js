bd079823aa2854b0e5dc1ace6f9f20cdb6807053
sheriallis <shrdson@gmail.com>
2019-08-02 23:18:13 +0200

    Add react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class App extends React.Component {
+  render() {
+    return <h1>REST Countries API</h1>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
