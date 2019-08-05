ff7a35595d2db42ebdb1095b1010d30ab4f40b5c
sheriallis <shrdson@gmail.com>
2019-08-05 23:51:27 +0200

    Add react hooks

---
+++ src/components/CountriesHome.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import CountryCard from "./CountryCard";
+const axios = require("axios");
+
+function CountriesHome() {
+  const [countries, setCountryData] = useState([]);
+
+  useEffect(() => {
+    getAllCountries();
+  }, []);
+
+  const getAllCountries = () => {
+    axios.get(`https://restcountries.eu/rest/v2/all`).then(res => {
+      const countries = res.data;
+      setCountryData(countries);
+    });
+  };
+
+  return (
+    <div className={`countries-grid`}>
+      <div className="countries-grid__inner">
+        {countries.map(country => (
+          <CountryCard
+            name={country.name}
+            key={country.name}
+            population={country.population}
+            region={country.region}
+            capital={country.capital}
+            flag={country.flag}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CountriesHome;
