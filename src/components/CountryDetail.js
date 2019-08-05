cd417c89552fac9709a9a8a10e14270c66014525
sheriallis <shrdson@gmail.com>
2019-08-05 23:50:36 +0200

    Add country detail route

---
+++ src/components/CountryDetail.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import "../index.css";
+const axios = require("axios");
+
+function CountryDetail() {
+  return (
+    <div>
+      <h1>Country</h1>
+    </div>
+  );
+}
+
+export default CountryDetail;
