4be833f4c2e2878ab7e15cb65cf0df1bb865a8d3
sheriallis <shrdson@gmail.com>
2019-08-08 20:55:51 +0200

    Add styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,69 @@
 import React from "react";
 import { FaMoon, FaRegMoon } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-function SwitchToDark({ toggleTheme }) {
+const AppHeader = styled.header`
+  display: flex;
+  justify-content: center;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  position: relative;
+  z-index: 1;
+  background-color: ${props => props.theme.bg};
+  color: ${props => props.theme.fg};
+`;
+
+const Wrapper = styled.div`
+  padding: 25px 15px 30px 15px;
+  width: 90%;
+  max-width: 1920px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  color: ${props => props.theme.fg};
+  font-weight: 800;
+`;
+
+const ThemeSwitchBtn = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .icon {
+    margin-right: 20px;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+function ThemeSwitcher({ lightTheme, toggleTheme }) {
   return (
-    <div className="app-header__mode" onClick={toggleTheme}>
-      <FaRegMoon className="app-header__icon" />
-      Dark Mode
-    </div>
+    <ThemeSwitchBtn onClick={toggleTheme}>
+      {lightTheme ? (
+        <FaRegMoon className="icon" />
+      ) : (
+        <FaMoon className="icon" />
+      )}
+      {lightTheme ? "Dark Mode" : "Light Mode"}
+    </ThemeSwitchBtn>
   );
 }
 
-function SwitchToLight({ toggleTheme }) {
+function Header({ lightTheme, toggleTheme }) {
   return (
-    <div className="app-header__mode" onClick={toggleTheme}>
-      <FaMoon className="app-header__icon" />
-      Light Mode
-    </div>
-  );
-}
-
-function Header({ theme, toggleTheme }) {
-  return (
-    <header className={`app-header app-header--${theme}`}>
-      <div className="app-header__inner">
+    <AppHeader>
+      <Wrapper>
         <Link to="/">
-          <h1 className="app-title">Where in the world?</h1>
+          <Title>Where in the world?</Title>
         </Link>
-        {theme === "light" ? (
-          <SwitchToDark toggleTheme={toggleTheme} />
-        ) : (
-          <SwitchToLight toggleTheme={toggleTheme} />
-        )}
-      </div>
-    </header>
+        <ThemeSwitcher lightTheme={lightTheme} toggleTheme={toggleTheme} />
+      </Wrapper>
+    </AppHeader>
   );
 }
 
