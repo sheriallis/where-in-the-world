78d1ad938cb9397f6d927530ad93850505172f05
sheriallis <shrdson@gmail.com>
2019-08-08 22:23:38 +0200

    Change background prop

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const AppHeader = styled.header`
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
   position: relative;
   z-index: 1;
-  background-color: ${props => props.theme.bg};
+  background-color: ${props => props.theme.elements};
   color: ${props => props.theme.fg};
 `;
 
