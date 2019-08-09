6065a5794efd0f344ad7269aa1fe818081625a2c
sheriallis <shrdson@gmail.com>
2019-08-09 21:54:22 +0200

    Add styled components and desktop styles

--- src/pages/CountryDetail.js
+++ src/pages/CountryDetail.js
@@ -17,7 +17,7 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   font-size: 16px;
-  width: 90%;
+  padding: 16px;
 
   a {
     margin-top: 60px;
@@ -35,9 +35,20 @@ const CountryFlag = styled.img`
 const CountryTitle = styled.h2`
   font-weight: 800;
   margin-bottom: 24px;
+  @media (min-width: 1440px) {
+    grid-column: 1 / 3;
+  }
 `;
 
-const CountryInfo = styled.div``;
+const CountryInfo = styled.div`
+  @media (min-width: 1440px) {
+    display: grid;
+    grid-template-rows: 60px 1fr 1fr;
+    grid-template-columns: repeat(2, 1fr);
+    margin-top: 60px;
+    margin-left: 60px;
+  }
+`;
 
 const CountryInfoList = styled.ul`
   list-style: none;
@@ -45,6 +56,9 @@ const CountryInfoList = styled.ul`
 
   span {
     font-weight: 600;
+  }
+  @media (min-width: 1440px) {
+    margin-right: 90px;
   }
 `;
 
@@ -56,6 +70,18 @@ const BorderCountries = styled.div`
 
   a {
     margin-right: 16px;
+  }
+  @media (min-width: 1440px) {
+    grid-row: 3 / 4;
+    grid-column: 1 / 3;
+  }
+`;
+
+const CountryData = styled.div`
+  @media (min-width: 1440px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 `;
 
@@ -96,47 +122,50 @@ export default function CountryDetail({ match }) {
         <Link to="/">
           <Button text={"Back"} />
         </Link>
-        <CountryFlag src={flag} alt={`flag for ${name}`} />
-        <CountryInfo>
-          <CountryTitle>{name}</CountryTitle>
-          <CountryInfoList>
-            <li>
-              <span>Native name: </span> {nativeName}
-            </li>
-            <li>
-              <span>Population: </span>
-              {population && population.toLocaleString()}
-            </li>
-            <li>
-              <span>Region: </span> {region}
-            </li>
-            <li>
-              <span>Sub region: </span> {subregion}
-            </li>
-            <li>
-              <span>Capital: </span> {capital}
-            </li>
-          </CountryInfoList>
-          <CountryInfoList>
-            <li>
-              <span>Top level domain: </span> {topLevelDomain}
-            </li>
-            <li>
-              <span>Currencies: </span>
-              {currencies &&
-                currencies.map(currency => currency.name).join(", ")}
-            </li>
-            <li>
-              <span>Languages: </span>
-              {languages && languages.map(language => language.name).join(", ")}
-            </li>
-          </CountryInfoList>
-          <BorderCountries>
-            <h3>Border Countries: </h3>
-            {borders &&
-              borders.splice(0, 3).map(country => <Button text={country} />)}
-          </BorderCountries>
-        </CountryInfo>
+        <CountryData>
+          <CountryFlag src={flag} alt={`flag for ${name}`} />
+          <CountryInfo>
+            <CountryTitle>{name}</CountryTitle>
+            <CountryInfoList>
+              <li>
+                <span>Native name: </span> {nativeName}
+              </li>
+              <li>
+                <span>Population: </span>
+                {population && population.toLocaleString()}
+              </li>
+              <li>
+                <span>Region: </span> {region}
+              </li>
+              <li>
+                <span>Sub region: </span> {subregion}
+              </li>
+              <li>
+                <span>Capital: </span> {capital}
+              </li>
+            </CountryInfoList>
+            <CountryInfoList>
+              <li>
+                <span>Top level domain: </span> {topLevelDomain}
+              </li>
+              <li>
+                <span>Currencies: </span>
+                {currencies &&
+                  currencies.map(currency => currency.name).join(", ")}
+              </li>
+              <li>
+                <span>Languages: </span>
+                {languages &&
+                  languages.map(language => language.name).join(", ")}
+              </li>
+            </CountryInfoList>
+            <BorderCountries>
+              <h3>Border Countries: </h3>
+              {borders &&
+                borders.splice(0, 3).map(country => <Button text={country} />)}
+            </BorderCountries>
+          </CountryInfo>
+        </CountryData>
       </Wrapper>
     </CountryDetailSection>
   );
