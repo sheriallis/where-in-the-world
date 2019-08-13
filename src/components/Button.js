e86629f4a4ca280299940a58824dc770347b359a
sheriallis <shrdson@gmail.com>
2019-08-13 15:44:11 +0200

    Use span tag instead of a tag for buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledButton = styled.a`
+const StyledButton = styled.span`
   padding: 12px 40px;
   border-radius: 5px;
   box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
   font-weight: 600;
   background-color: ${props => props.theme.elements};
   color: ${props => props.theme.fg};
+  margin-right: 16px;
 `;
 
-export default function Button({ text, src }) {
-  return <StyledButton href={src}>{text}</StyledButton>;
+export default function Button({ text }) {
+  return <StyledButton>{text}</StyledButton>;
 }
