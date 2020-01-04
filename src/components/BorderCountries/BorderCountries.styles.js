9705efbe3ddba183f93ecb096952c2e8f3730e39
sheriallis <shrdson@gmail.com>
2020-01-04 12:59:32 +0100

    Add shared button styles

--- src/components/BorderCountries/BorderCountries.styles.js
+++ src/components/BorderCountries/BorderCountries.styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+import { sharedButtonStyles } from "../Button/Button.styles";
 import { Link } from "react-router-dom";
 import { Breakpoints } from "../../styles/Breakpoints";
 
@@ -16,17 +17,9 @@ export const StyledBorderCountries = styled.div`
 `;
 
 export const StyledLink = styled(Link)`
-  padding: 10px 40px;
-  border-radius: 5px;
-  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+  ${sharedButtonStyles}
   font-weight: 600;
-  background-color: ${props => props.theme.elements};
-  color: ${props => props.theme.fg};
   margin: 0 16px 16px 0;
-  display: inline-flex;
-  justify-content: space-between;
-  align-items: center;
-  transition: background-color 0.2s ease-in-out;
 
   &:hover {
     background-color: ${props => props.theme.hoverBg};
--- src/components/Button/Button.styles.js
+++ src/components/Button/Button.styles.js
@@ -1,20 +1,24 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const StyledButton = styled.button`
+export const sharedButtonStyles = css`
   padding: 10px 40px;
   border: none;
   border-radius: 5px;
   box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
-  font-size: 16px;
-  font-weight: 400;
   background-color: ${props => props.theme.elements};
   color: ${props => props.theme.fg};
-  margin-right: 16px;
-  margin-bottom: 16px;
-  display: inline-flex;
   justify-content: space-between;
   align-items: center;
   transition: background-color 0.2s ease-in-out;
+`;
+
+export const StyledButton = styled.button`
+  ${sharedButtonStyles}
+  font-size: 16px;
+  font-weight: 400;
+  margin-right: 16px;
+  margin-bottom: 16px;
+  display: inline-flex;
 
   &:hover {
     background-color: ${props => props.theme.hoverBg};
